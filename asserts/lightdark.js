03863c3a7a241a46734fb71ea666115c4b4f46d5
William Norman <lordcrekit@gmail.com>
2017-04-12 17:08:13 -0400

    Fixed lightdark to match standard

--- asserts/lightdark.js
+++ asserts/lightdark.js
@@ -1,7 +1,7 @@
 /**
- * Sets the colorscheme for the website, based on stored settings. 
+ * Sets the colorscheme for the website, based on stored settings.
  */
-function SetTheMood() {
+function SetTheMood () {
   var dark = window.localStorage.getItem('dark') === 'true'
   document.getElementsByTagName('BODY')[0].className = dark
     ? 'night-theme'
@@ -10,14 +10,14 @@ function SetTheMood() {
     ? 'Come to the light!'
     : 'Join the dark side!'
   var count = parseInt(window.localStorage.getItem('trumpCount')) || 0
-  document.getElementById('trumpCount').textContent = count;
+  document.getElementById('trumpCount').textContent = count
 }
 
 /**
  * Pull a 180 out of nowhere on the colorscheme for the website. The change is
  *  preserved in stored settings.
  */
-function TrumpOnLightingPolicy() {
+function TrumpOnLightingPolicy () {
   window.localStorage.setItem('dark', window.localStorage.getItem('dark') !== 'true')
   window.localStorage.setItem('trumpCount',
       (parseInt(window.localStorage.getItem('trumpCount')) || 0) + 1)
